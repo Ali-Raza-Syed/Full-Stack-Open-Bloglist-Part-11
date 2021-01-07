26259d2d5800b99a50fbe8aeb4c4f759ac42b140
achoo486 <saliraza486@gmail.com>
2021-01-07 11:23:53 +0500

    heroku commit

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./contollers/testing')
   app.use('/api/testing', testingRouter)
